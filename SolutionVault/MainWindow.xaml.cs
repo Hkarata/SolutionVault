9a4e29bc4106d6c5ea08519ddce732cb162a738b
hei karata <herishine00@gmail.com>
2024-04-20 03:10:33 +0300

    `Implemented new UI and added new XAML pages`
     Updated the UI in App.xaml and MainWindow.xaml, introducing new theme dictionaries and replacing the previous Window and StackPanel with ex:WindowEx and a Grid. MainWindow.xaml.cs was updated to handle the new UI elements. Added references to three new XAML pages (Home.xaml, Settings.xaml, Welcome.xaml) in SolutionVault.csproj and added these pages to the project with their corresponding code-behind files.

--- SolutionVault/MainWindow.xaml.cs
+++ SolutionVault/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using System;
+using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -8,16 +12,49 @@ namespace SolutionVault
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : WinUIEx.WindowEx
     {
         public MainWindow()
         {
             this.InitializeComponent();
+            this.SystemBackdrop = new MicaBackdrop();
+            ExtendsContentIntoTitleBar = true;
+            this.SetTitleBarBackgroundColors(Microsoft.UI.Colors.Transparent);
+            PersistenceId = "MainWindow";
+            navigationView.Loaded += NavigationView_Loaded;
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            navigationView.SelectedItem = navigationView.MenuItems[0];
         }
+
+        private void NavigationView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                contentFrame.Navigate(typeof(Pages.Settings));
+            }
+            else
+            {
+                var selectedItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
+                if (selectedItem != null)
+                {
+                    string selectedItemTag = ((string)selectedItem.Tag);
+                    sender.Header = selectedItem.Content;
+                    string pageName = "SolutionVault.Pages." + selectedItemTag;
+                    Type pageType = Type.GetType(pageName);
+                    contentFrame.Navigate(pageType);
+                }
+            }
+            sender.IsBackEnabled = contentFrame.CanGoBack;
+        }
+
+        private void NavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (contentFrame.CanGoBack)
+                contentFrame.GoBack();
+        }
+
     }
 }
